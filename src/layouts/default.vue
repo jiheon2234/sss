c2354bfd93a47f99e2d040b493a1e128e5d17f2b
jiheon2234 <jiheon2234@gmail.com>
2024-04-29 19:40:25 +0900

    tmp

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,24 +1,51 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-header bordered class="bg-white text-grey-9">
+      <q-toolbar>
+        <q-btn flat dense>
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="logo.png" />
+            </q-avatar>
+            별다줄
+          </q-toolbar-title>
+        </q-btn>
+        <q-space />
+        <q-btn stretch flat label="Home" to="/" />
+        <q-btn stretch flat label="About" />
+        <q-btn stretch flat label="Pricing" />
+        <q-btn stretch flat label="Developers" />
+        <q-separator class="q-my-sm q-mr-sm" vertical />
+        <q-btn unelevated rounded color="primary" label="Sign In/ Sign up" />
+        <q-avatar color="warning" text-color="white">J</q-avatar>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container :style="pageContainerStyles">
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
-          Title
+          <div>Title</div>
         </q-toolbar-title>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+    </q-footer>
   </q-layout>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+console.log(route.meta?.width);
+
+const pageContainerStyles = computed(() => ({
+  maxWidth: route.meta?.width || '1920px',
+  margin: '0 auto',
+}));
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,80 @@
 <template>
-  <div>
-    <div class="text-h4">INDEx</div>
-  </div>
+  <q-page>
+    <div class="row window-height">
+      <div class="col flex flex-center">
+        <q-card flat :style="{ minWidth: '1000px' }">
+          <q-card-section>
+            <div
+              class="text-h1 text-weight-bold text-center flex justify-center items-center"
+            >
+              <span>별</span>
+              <span class="highlight">걸</span>
+              <span :style="$q.screen.gt.sm ? { 'margin-right': '1.5rem' } : {}"
+                >다</span
+              >
+              <span>줄</span>
+              <span class="highlight">이네</span>
+            </div>
+            <div class="text-subtitle2 text-grey">와주셔서 감사합니다☺️</div>
+          </q-card-section>
+          <q-card-section>
+            <q-form>
+              <div class="q-gutter-y-lg row">
+                <q-input
+                  filled
+                  label="이메일"
+                  hint="이메일을 입력해주세요~~"
+                ></q-input>
+                <q-input
+                  filled
+                  label="비밀번호"
+                  hint="비밀번호를 입력해주세요~~"
+                ></q-input>
+              </div>
+              <q-btn
+                label="로그인하기"
+                unelevated
+                color="primary"
+                size="lg"
+                class="full-width q-mt-lg"
+              />
+
+              <q-btn
+                label="회원가입"
+                flat
+                size="lg"
+                class="full-width q-mt-md"
+                to="/auth/sign-up"
+              />
+              <q-btn
+                label="대시보드로 이동"
+                flat
+                size="lg"
+                class="full-width q-mt-xs"
+                to="/"
+              />
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.highlight {
+  font-size: 1rem;
+  color: $primary;
+  margin: 0.5rem;
+
+  body.screen--xs &,
+  body.screen--sm & {
+    display: none;
+  }
+}
+</style>
 
 <route lang="yaml">
 name: home-page
