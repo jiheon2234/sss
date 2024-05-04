54ebad9f80f56b635c6899a7beb0075127762259
jiheon2234 <jiheon2234@gmail.com>
2024-05-04 19:14:11 +0900

    tmptmp

---
+++ src/pages/components/URLDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-dialog
+    :model-value="modelValue"
+    @update:model-value="val => $emit('update:modelValue', val)"
+    transition-show="slide-up"
+    transition-hide="slide-down"
+  >
+    <q-card :style="{ width: '400px' }">
+      <q-card-section class="flex">
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section class="q-px-xl q-pb-xl">
+        <div class="text-h5 text-center text-weight-bold q-mb-xl">URL을 줄였음</div>
+        {{ shortURL }}
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  shortURL: {
+    type: String,
+    default: 'sssssss',
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,89 +1,14 @@
 <template>
-  <q-page>
-    <div class="row window-height">
-      <div
-        class="col flex justify-center items-center min-width-200px"
-        style="min-width: 1000px"
-      >
-        <q-card flat>
-          <div class="row">
-            <div>
-              <q-card-section>
-                <div
-                  class="text-h1 text-weight-bold text-center flex justify-center items-center q-mx-md"
-                >
-                  <span>별</span>
-                  <span
-                    class="highlight"
-                    style="margin-left: 0.5rem; margin-right: 0.5rem"
-                    >걸</span
-                  >
-                  <span>다</span>
-                  <span style="margin-left: 0.5rem">줄</span>
-                  <span class="highlight" style="margin-left: 0.5rem"
-                    >이네</span
-                  >
-                </div>
-              </q-card-section>
-            </div>
-          </div>
-          <div class="row">
-            <q-card-section>
-              <q-form>
-                <div class="q-gutter-sm col-12 row items-center">
-                  <q-input
-                    class="col-grow"
-                    rounded
-                    outlined
-                    v-model="inputUrl"
-                    :label="
-                      inputUrl == ''
-                        ? 'URL을 입력해주세요'
-                        : isValidUrl
-                        ? 'URL'
-                        : '올바른 URL 형식이 아닙니다'
-                    "
-                    :color="
-                      inputUrl == '' ? 'primary' : isValidUrl ? 'green' : 'red'
-                    "
-                  />
-                  <q-btn color="red" round label="GO!" :disable="!isValidUrl" />
-                </div>
-              </q-form>
-            </q-card-section>
-          </div>
-        </q-card>
-      </div>
-    </div>
+  <q-page class="q-pa-xl">
+    <ShortenURL />
   </q-page>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-const inputUrl = ref('');
-
-const isValidUrl = computed(() => {
-  const urlPattern = new RegExp(
-    '^(https?:\\/\\/)([\\da-z\\.-]+\\.[a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$',
-    'i',
-  );
-  console.log(urlPattern.test(inputUrl.value));
-  return urlPattern.test(inputUrl.value);
-});
+import ShortenURL from './components/ShortenURL.vue';
 </script>
 
-<style lang="scss" scoped>
-.highlight {
-  font-size: 1rem;
-  color: $primary;
-  margin: 0.5rem;
-
-  body.screen--xs &,
-  body.screen--sm & {
-    display: none;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
 
 <route lang="yaml">
 name: home-page
